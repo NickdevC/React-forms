5310b9e8b2890e0e77b5d7bb2cd75a34ee0335d5
NickdevC <nick_coles@msn.com>
2023-04-01 12:41:21 +0000

    Setup initial workspace file structure

---
+++ my-app/src/components/ControlledForm.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class ControlledForm extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            first
+        }
+    }
+    render() {
+        return (
+            <div>
+                <form>
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default ControlledForm
