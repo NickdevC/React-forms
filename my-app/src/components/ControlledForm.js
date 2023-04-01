8c7b3bc8b1b5e5fccb2d39e7cabe56e41efd029a
NickdevC <nick_coles@msn.com>
2023-04-01 13:19:59 +0000

    Add textarea field to form

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -16,21 +16,30 @@ export class ControlledForm extends Component {
     render() {
         return (
             <div>
+                <h2>Please fill out the form below:</h2>
                 <form>
-                    <label htmlFor='id-name'>Your name:</label>
-                    <input 
-                        value={this.state.name} 
-                        onChange={this.handleNameChange}
-                        id='id-name' 
-                        name='name' 
-                        type="text" 
-                    />
-                    <label htmlFor='id-category'>Query category:</label>
-                    <select id='id-category' name='category'>
-                        <option>Website issue</option>
-                        <option>Order issue</option>
-                        <option>General enquiry</option>
-                    </select>
+                    <div>
+                        <label htmlFor='id-name'>Your name:</label>
+                        <input 
+                            value={this.state.name} 
+                            onChange={this.handleNameChange}
+                            id='id-name' 
+                            name='name' 
+                            type="text" 
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='id-category'>Query category:</label>
+                        <select id='id-category' name='category'>
+                            <option value="website">Website issue</option>
+                            <option value="order">Order issue</option>
+                            <option value="general">General enquiry</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor='id-comments'>Comments:</label>
+                        <textarea id='id-comments' name='comments' />
+                    </div>
                     <input type="submit" value="Submit" />
                 </form>
             </div>
